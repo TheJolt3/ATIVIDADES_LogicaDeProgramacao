187f7c7723b36397d1d30d3f1a87f2a1460ddddd
TheJolt3 <brunoax05@gmail.com>
2026-01-15 07:41:47 -0300

    Update app.js
    Substituindo funções simples para algo mais legível e adicionando a função de Sortear
    Também acrescentando o uso de (Math.random())

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -1,10 +1,21 @@
 function sortear() {
-    let quantidade = document.getElementById('quantidade').value;
-    let de = document.getElementById('de').value;
-    let ate = document.getElementById('ate').value;
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
 
-    alert(`Quantidade: ${quantidade}`);
-    alert(`Do número: ${de}`);
-    alert(`Quantidade: ${ate}`);
-    
+let sorteados = [];
+let numero;
+
+// sorteia os números
+for(let i = 0; i < quantidade; i++) {
+    numero = obterNumeroAleatorio(de, ate);
+    sorteados.push(numero);
+}
+
+alert(sorteados);
+}
+
+// função para obter um número aleatório entre minimo e máximo
+function obterNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
