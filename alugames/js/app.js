13c8ea92984faf7219d279abc588724e51109500
TheJolt3 <brunoax05@gmail.com>
2026-01-20 07:33:41 -0300

    Update app.js
    Adicionando variáveis para ativar o botão entre "devolver e alugar"

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -1,8 +1,18 @@
+// Função para alterar o status do jogo (alugado ou disponível)
 function alterarStatus(id) {
     let gameClicado = document.getElementById(`game-${id}`);
-    let imagem = gameClicado.querySelector('.dashboard__items__item');
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
     let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
 
-    alert(nomeJogo.textContent);
+    // Alterna a classe e o texto do botão com base no status atual
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
+    }
 }
