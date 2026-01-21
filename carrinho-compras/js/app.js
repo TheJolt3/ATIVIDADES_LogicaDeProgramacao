9473d31e93330b05f6e3cb5570cb5f8fbc3e601a
TheJolt3 <brunoax05@gmail.com>
2026-01-21 08:53:12 -0300

    Update app.js

---
+++ carrinho-compras/js/app.js
@@ -0,0 +1,23 @@
+function adicionar() {
+//recuperar valores nome do produto, quantidade e valor
+let produto = document.getElementById('produto').value;
+let nomeProduto = produto.split('-')[0];
+let valorUnitario = produto.split('R$')[1];
+let quantidade = document.getElementById('quantidade');
+alert(nomeProduto);
+alert(valorUnitario);
+alert(quantidade.value);
+let preco = quantidade. value * valorUnitario;
+alert(preco);
+
+
+//calcular o preço, o nosso subtotal
+//adicionar no carrinho 
+// atualizar o valor total
+
+
+}
+
+function limpar() {
+
+}
