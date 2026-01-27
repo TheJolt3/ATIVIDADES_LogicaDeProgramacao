a0aafad4bba0c989c156de42e6d82cce0b9e3196
TheJolt3 <brunoax05@gmail.com>
2026-01-27 07:46:51 -0300

    Update app.js
    Realizando compra para o tipo Pista

--- ingresso/js/app.js
+++ ingresso/js/app.js
@@ -1,7 +1,21 @@
+// Função para comprar ingresso
 function comprar() {
     let tipo = document.getElementById('tipo-ingresso');
-    let qtd = document.getElementById('qtd');
+    let qtd = parseInt(document.getElementById('qtd').value);
 
-    alert(tipo.value);
-    alert(qtd.value);
+    if(tipo.value == 'pista') {
+        comprarPista(qtd);
+    }
+}
+
+// Função para comprar ingresso do tipo pista
+function comprarPista(qtd) {
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+    if (qtd > qtdPista) {
+        alert('Quantidade indisponível para tipo pista');
+    } else {
+        qtdPista = qtdPista - qtd;
+        document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada com sucesso!');
+    }
 }
