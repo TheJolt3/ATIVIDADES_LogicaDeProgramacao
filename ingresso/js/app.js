70fd721f9589d711e72b64eb457b9d385d5c7889
TheJolt3 <brunoax05@gmail.com>
2026-01-28 07:28:57 -0300

    Update app.js

--- ingresso/js/app.js
+++ ingresso/js/app.js
@@ -1,14 +1,16 @@
-// Função para comprar ingresso
 function comprar() {
     let tipo = document.getElementById('tipo-ingresso');
-    let qtd = parseInt(document.getElementById('qtd').value);
+    let qtd = document.getElementById('qtd').value;
 
-    if(tipo.value == 'pista') {
+    if (tipo.value == 'pista') {
         comprarPista(qtd);
+    } else if (tipo.value == 'superior') {
+        comprarSuperior(qtd);
+    } else {
+        comprarInferior(qtd);
     }
 }
 
-// Função para comprar ingresso do tipo pista
 function comprarPista(qtd) {
     let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
     if (qtd > qtdPista) {
@@ -16,6 +18,28 @@ function comprarPista(qtd) {
     } else {
         qtdPista = qtdPista - qtd;
         document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada com sucesso!');
+    }
+}
+
+function comprarSuperior(qtd) {
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+    if (qtd > qtdSuperior) {
+        alert('Quantidade indisponível para tipo superior');
+    } else {
+        qtdSuperior = qtdSuperior - qtd;
+        document.getElementById('qtd-superior').textContent = qtdSuperior;
+        alert('Compra realizada com sucesso!');
+    }
+}
+
+function comprarInferior(qtd) {
+    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+    if (qtd > qtdInferior) {
+        alert('Quantidade indisponível para tipo inferior');
+    } else {
+        qtdInferior = qtdInferior - qtd;
+        document.getElementById('qtd-inferior').textContent = qtdInferior;
         alert('Compra realizada com sucesso!');
     }
 }
